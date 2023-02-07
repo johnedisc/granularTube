0c1a83c7b8aeb726413c9f44d60320b61bb8a18b
johnedisc <christopher.johnedis@gmail>
2023-02-07 14:48:30 -0800

    begin migrating api call to separate class

---
+++ src/js/gifCall.js
@@ -0,0 +1,20 @@
+export default class gif {
+  static searchAPI(query) {
+    return new Promise(function(resolve,reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?q=${query}&limit=5&api_key=${process.env.API_KEY}`;
+      request.addEventListener("loadend", function () {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve([response,'response']);
+        } else {
+          reject([this,response,query]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    }
+  }
+
+
+}
