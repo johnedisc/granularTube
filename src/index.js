0beb7001cc7c6ea0193b2f41b64e7a9c48a9f29d
johnedisc <christopher.johnedis@gmail.com>
2023-02-08 14:04:59 -0800

    WIP work on building out the promises
    Co-authored-by: John Diggins <rohnfiggins@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,39 +1,25 @@
 import './css/styles.css';
-import { Granular } from './js/fetchTube';
+import { callAPI } from './js/callAPI';
 
+function fetchTube(query) {
+  callAPI(query)
+    .then((response) => {
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    });
+  
+}
 
-
-// async await fetch
-// async function fetchTube(q) {
-//   const response = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&key=${process.env.API_KEY}`);
-
-//   const data = await response.json();
-//   return data
-//   // const video = data.items[0].id.videoId;
-//   // console.log(typeof(video));
-//   // return video;
+// function printResult(apiObject) {
+//   document.getElementById("output").innerHTML = '';
 // }
-async function fetchTube(q) {
-  const response =  await Granular.callAPI(q);
-  if (response.items) {
-    console.log(response.items[0].id.videoId);
-    printResult(response);
-  } else {
-    console.log(response);
-  }
-}
-
-function printResult(obj) {
-  document.getElementById("output").innerHTML = obj.items[0].id.videoId;
-  // console.log(fetchTube(q));
-
-}
 
 
 function handleFormSubmission(e) {
   e.preventDefault();
-  const q = document.getElementById("search").value;
-  fetchTube(q);
+  const query = document.getElementById("search").value;
+  fetchTube(query);
 }
 
 window.addEventListener("load", function() {
---
+++ src/js/callAPI.js
@@ -0,0 +1,21 @@
+export function callAPI(query) {
+  return new Promise(function(resolve, reject) {
+    if (query) {
+      resolve(query)
+    } else {
+      reject(`search query doesn't exist`)
+    }
+  });
+  // try {
+  //   const response = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&key=${process.env.API_KEY}`);
+  //   const data = await response.json();
+  //   if (!response.ok) {
+  //     const errMessage = `${response.status} ${response.statusText}
+  //     ${data.message}`;
+  //     throw new Error(errMessage);
+  //   }
+  //   return data;
+  // } catch(error) {
+  //   return error;
+  // }
+}
