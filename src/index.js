5394562eb13f20a9e5259bf4a399d2574fc3a22b
johnedisc <christopher.johnedis@gmail.com>
2023-02-08 11:20:15 -0800

    move fetch call to separate file
    Co-authored-by: John Diggins <rohnfiggins@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import './css/styles.css';
+import { Granular } from './js/fetchTube';
 
 
 
 // async await fetch
-async function fetchTube(q) {
-  const response = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&key=${process.env.API_KEY}`);
+// async function fetchTube(q) {
+//   const response = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&key=${process.env.API_KEY}`);
 
-  const data = await response.json();
-  const video = data.items[0].id.videoId;
-  console.log(typeof(video));
-  return video;
+//   const data = await response.json();
+//   return data
+//   // const video = data.items[0].id.videoId;
+//   // console.log(typeof(video));
+//   // return video;
+// }
+async function fetchTube(q) {
+  const response =  await Granular.fetchTube(q);
+  if (response.items) {
+    console.log(response.items[0].id.videoId);
+  } else {
+    console.log(response);
+  }
 }
 
 
@@ -17,8 +27,8 @@ function handleFormSubmission(e) {
   e.preventDefault();
   const q = document.getElementById("search").value;
   fetchTube(q);
-  document.getElementById("output").innerHTML = ;
-  console.log(fetchTube(q));
+  // document.getElementById("output").innerHTML = ;
+  // console.log(fetchTube(q));
 }
 
 window.addEventListener("load", function() {
---
+++ src/js/fetchTube.js
@@ -0,0 +1,17 @@
+export class Granular {
+  static async fetchTube(q) {
+    try {
+      
+      const response = await fetch (`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&key=${process.env.API_KEY}`);
+      const data = await response.json();
+      if (!response.ok) {
+        const errMessage = `${response.status} ${response.statusText}
+        ${data.message}`;
+        throw new Error(errMessage);
+      }
+      return data;
+    } catch(error) {
+      return error;
+    }
+  }
+}
